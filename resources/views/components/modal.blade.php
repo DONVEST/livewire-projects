9a8e529b68534284173b4ae1e213d668dc6738fc
DONVEST <Theopensly@gmail.com>
2025-03-18 11:45:40 +0100

    new update

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,13 +1,18 @@
+@props(['title'])
 <div x-data="{show:false}" x-show="show" 
 x-on:open-modal.window="show = true" 
 x-on:close-modal.window="show = false"
 x-on:keydown.escape.window="show = false"
 class="fixed z-50 inset-0">
 
-<div class="fixed inset-0 bg-gray-300 opacity-400 "></div>
+<div class="fixed inset-0 bg-gray-300 opacity-400 " x-on:click="$dispatch('close-modal')"></div>
 
 <div class="bg-white rounded m-auto fixed inset-0 max-w-2xl" style="max-height:500px;">
-    <div>Header</div>
+    @if (isset($title))
+        <div class="py-3 flex items-center justify-center">
+            {{$title ?? ''}}
+        </div>
+    @endif
     <div>Body</div>
 </div>
 
