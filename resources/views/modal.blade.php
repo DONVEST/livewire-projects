52dd4d18623c6e2348d08bb47e14cadf17117a61
DONVEST <Theopensly@gmail.com>
2025-03-18 13:01:30 +0100

    updating

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todo App template</title>
+    <title>{{env('APP_NAME')}}</title>
+    <link rel="stylesheet" href='{{asset('asset/css/btn_preloader.css')}}'>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
@@ -14,16 +15,57 @@
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+    
 </head>
 
 <body class="p-5">
 
-    <x-modal></x-modal>
+    
+    
+    
+    
+    
+    
+    
+    
+    <x-modal title="modal 1">
+        <x-slot:body>
+            <livewire:register />
+        </x-slot:body>
+    </x-modal>
 
     <button x-data x-on:click="$dispatch('open-modal')" 
     class="px-3 py-1 bg-teal-500 text-white rounded">Open Modal</button>
     
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     {{-- Sweet Alert --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
